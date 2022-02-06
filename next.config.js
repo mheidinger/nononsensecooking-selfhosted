3f15a7c9641766e2c82da8ca6a55d999bb1b059e
Max Heidinger <mail@max-heidinger.de>
2022-02-06 19:46:58 +0100

    Read docker secret for aws env

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 const { i18n } = require("./next-i18next.config");
+const webpack = require('webpack');
+const dotenv = require('dotenv');
+
+const { parsed: awsEnv } = dotenv.config({
+    path:'/run/secrets/nononsensecooking-aws'
+});
 
 module.exports = {
   i18n,
@@ -9,5 +15,11 @@ module.exports = {
   images: {
     formats: ["image/avif", "image/webp"],
     domains: [process.env.S3_DOMAIN],
+  },
+  webpack(config) {
+    if (awsEnv) {
+      config.plugins.push(new webpack.EnvironmentPlugin(awsEnv))
+    }
+    return config
   },
 };
